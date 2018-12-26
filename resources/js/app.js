0da0eba8c75ddb1b53d6ac30cb6f14482418b603
yclgitc9@gmail.com <yclgitc9@gmail.com>
2018-12-26 18:03:26 +0300

    end of video 10

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
-
+ 
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -8,6 +8,11 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+import Buefy from 'buefy';
+
+
+Vue.use(Buefy);
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +25,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,6 +33,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Project3.app
                 </div>
 
                 <div class="links">
